ccc436ea44dca45e6ad90fb75bb55b7ecd3bb11c
Silena Dangond <silenadangond1@gmail.com>
2021-09-02 21:14:18 -0500

    FrontEnd layout and database modification

--- backEnd/dataBase/querys.sql
+++ backEnd/dataBase/querys.sql
@@ -3,17 +3,17 @@ CREATE TABLE `users` (
   `name` varchar(45) DEFAULT NULL,
   `lastname` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `profile` varchar(45) DEFAULT NULL,
+  `cargo` varchar(45) DEFAULT NULL,
   `is_admin` tinyint DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) 
+);
 
 CREATE TABLE `regions` (
   `region_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`region_id`)
-)
+);
 
 CREATE TABLE `countries` (
   `country_id` int NOT NULL AUTO_INCREMENT,
@@ -22,7 +22,7 @@ CREATE TABLE `countries` (
   PRIMARY KEY (`country_id`),
   KEY `region_id_idx` (`region_id`),
   CONSTRAINT `region_id_FK_region_countries` FOREIGN KEY (`region_id`) REFERENCES `regions` (`region_id`)
-)
+);
 
 CREATE TABLE `cities` (
   `city_id` int NOT NULL AUTO_INCREMENT,
@@ -31,7 +31,7 @@ CREATE TABLE `cities` (
   PRIMARY KEY (`city_id`),
   KEY `country_id_idx` (`country_id`),
   CONSTRAINT `country_id_FK_Countries_City` FOREIGN KEY (`country_id`) REFERENCES `countries` (`country_id`)
-)
+);
 
 CREATE TABLE `companies` (
   `company_id` int NOT NULL AUTO_INCREMENT,
@@ -43,7 +43,7 @@ CREATE TABLE `companies` (
   PRIMARY KEY (`company_id`),
   KEY `FK_city_companies_idx` (`city_id`),
   CONSTRAINT `FK_city_companies` FOREIGN KEY (`city_id`) REFERENCES `cities` (`city_id`)
-)
+);
 
 CREATE TABLE `contacts` (
   `contact_id` int NOT NULL AUTO_INCREMENT,
@@ -53,13 +53,13 @@ CREATE TABLE `contacts` (
   `company_id` int DEFAULT NULL,
   `city_id` int DEFAULT NULL,
   `address` varchar(45) DEFAULT NULL,
-  `profile` varchar(45) DEFAULT NULL,
+  `cargo` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`contact_id`),
   KEY `FK_company_contacts_idx` (`company_id`),
   KEY `FK_city_contacts_idx` (`city_id`),
   CONSTRAINT `FK_city_contacts` FOREIGN KEY (`city_id`) REFERENCES `cities` (`city_id`),
   CONSTRAINT `FK_company_contacts` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
-)
+);
 
 CREATE TABLE `channels` (
   `channel_id` int NOT NULL AUTO_INCREMENT,
@@ -96,5 +96,6 @@ CREATE TABLE `data_warehouse`.`prefe_cont_chann` (
     FOREIGN KEY (`preferences_id`)
     REFERENCES `data_warehouse`.`preferences` (`preferences_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION
+);
 
